c2a0a1c3e26e6b262a7a7ab1c55d975e39ab4f44
GitPatout <patout.julien.02@gmail.com>
2022-05-11 16:40:06 +0200

    nouveau api

--- my-api/src/App.js
+++ my-api/src/App.js
@@ -1,24 +1,26 @@
 import { useState, useEffect } from 'react';
 import "./App.css";
 
+
 export default function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [query, setQuery] = useState("")
 
 
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch('https://dummyjson.com/products');
         if (!response.ok) {
           throw new Error(
             `This is an HTTP error: The status is ${response.status}`
           );
         }
         let actualData = await response.json();
-        setData(actualData);
+        setData(actualData.products);
         setError(null);
       } catch (err) {
         setError(err.message);
@@ -35,14 +37,30 @@ export default function App() {
     {loading && <div>A moment please...</div>}
     {error && (
       <div>{`There is a problem fetching the post data - ${error}`}</div>
-    )}
+      )}
+      <input placeholder='Enter Post Title' onChange={event => setQuery(event.target.value)} />
     <ul>
       {data &&
-        data.map(({ id, title }) => (
+        data.filter(post => {
+          if (query === '') {
+            return post;
+          } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          }
+        }).map(({ id, title, description, thumbnail, price }) => (
           <li key={id}>
+            <img src={thumbnail} />
             <h3>{title}</h3>
+            <p>{description}</p>
+
+            <p>{price}</p>
           </li>
         ))}
     </ul>
-  </div>;
+
+
+
+
+    
+  </div>
 }
